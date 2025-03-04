d5639fa3e0f06cc9f2f2e70ceb70a43b50f23d74
e.karimi <abnoseek@gmail.com>
2025-03-04 15:14:46 +0330

    feat: adding primevue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import tailwindcss from "@tailwindcss/vite";
+import Aura from "@primeuix/themes/aura";
+
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   css: ["@/assets/css/style.css"],
   vite: {
     plugins: [tailwindcss()],
+  },
+  modules: ["@primevue/nuxt-module"],
+  primevue: {
+    options: {
+      theme: {
+        preset: Aura,
+      },
+    },
   },
 });
