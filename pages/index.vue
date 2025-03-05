499a6806c8b42840f30f3f19233d3dde224310ce
e.karimi <abnoseek@gmail.com>
2025-03-05 15:47:11 +0330

    feat: try to use base url

--- pages/index.vue
+++ pages/index.vue
@@ -47,48 +47,25 @@
       class="px-[1rem] md:px-[6rem] lg:px-[9.75rem] mt-[2rem] grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-6 w-full h-full bg-[#2C2E30] rounded-[16px] p-4"
       aria-label="Content Grid"
     >
-      <article
-        class="box-border flex flex-col items-start p-4 gap-4 w-full min-w-[20rem] min-h-[29rem] max-h-[33.8rem] border border-[#404244] rounded-[16px] mx-auto"
-        aria-labelledby="item1-title"
-      >
-        <img
-          class="w-full h-[29.31rem] p-[1rem] bg-amber-200 rounded-[.5rem]"
-          src=""
-          alt="image"
-        />
-        <p class="text-[1rem] pr-[1rem]">Description for item 1.</p>
-      </article>
-
-      <article
-        class="box-border flex flex-col items-center p-4 gap-4 w-full min-w-[20rem] min-h-[29rem] max-h-[33.8rem] border border-[#404244] rounded-[16px] mx-auto"
-        aria-labelledby="item2-title"
-      >
-        <h2 id="item2-title" class="text-lg font-semibold">Item 2 Title</h2>
-        <p>Description for item 2.</p>
-      </article>
-
-      <article
-        class="box-border flex flex-col items-center p-4 gap-4 w-full min-w-[20rem] min-h-[29rem] max-h-[33.8rem] border border-[#404244] rounded-[16px] mx-auto"
-        aria-labelledby="item3-title"
-      >
-        <h2 id="item3-title" class="text-lg font-semibold">Item 3 Title</h2>
-        <p>Description for item 3.</p>
-      </article>
-
-      <article
-        class="box-border flex flex-col items-center p-4 gap-4 w-full min-w-[20rem] min-h-[29rem] max-h-[33.8rem] border border-[#404244] rounded-[16px] mx-auto"
-        aria-labelledby="item4-title"
-      >
-        <h2 id="item4-title" class="text-lg font-semibold">Item 4 Title</h2>
-        <p>Description for item 4.</p>
-      </article>
+      <ItemCard
+        v-for="character in characters?.data?.results"
+        :key="character.id"
+        :to="`/character/${character.id}`"
+        :imgSrc="character.thumbnail.path + '.' + character.thumbnail.extension"
+        :altText="character.name"
+        :name="character.name"
+      />
     </main>
 
     <footer
       class="px-[1rem] md:px-[6rem] lg:px-[9.75rem] mt-[1.5rem] mb-[4rem] gap-6 w-full h-full bg-[#2C2E30] rounded-[16px]"
       aria-label="Pagination Controls"
     >
-      <Paginator :rows="10" :totalRecords="120"></Paginator>
+      <Paginator
+        @page="handlePageChange"
+        :rows="10"
+        :totalRecords="120"
+      ></Paginator>
     </footer>
   </div>
 </template>
@@ -97,7 +74,7 @@
 import { useMarvelApi } from "~/composables/useApi";
 import { ref } from "vue";
 import { useRouter } from "vue-router";
-
+const characters = ref();
 const searchQuery = ref("");
 const router = useRouter();
 
@@ -107,18 +84,41 @@ const handleInputChange = (event) => {
 
 const handleSearch = (event) => {
   event.preventDefault();
-  if (searchQuery.value.trim()) {
-    router.push({ path: "/search", query: { q: searchQuery.value } });
-  }
+  initialRequest(12, 0, searchQuery.value);
 };
 
-// const { data, pending, error } = await useAsyncData("characters", () =>
-//   useMarvelApi("comics")
-// );
+// const { data, pending, error } = await useMarvelApi("characters", {
+//   query: {
+//     limit: 12,
+//     offset: 0,
+//   },
+// });
 
 // const { data, pending, error } = await useAsyncData("search-characters", () =>
 //   useMarvelApi("characters", { query: { nameStartsWith: "ca" } })
 // );
+
+const initialRequest = async (limit = 12, offset = 0, searchQuery = "") => {
+  const { data } = await useAsyncData("search-characters", () =>
+    useMarvelApi("characters", {
+      query: {
+        limit: limit || 11,
+        offset: offset || 0,
+        ...(searchQuery && { nameStartsWith: searchQuery }),
+      },
+    })
+  );
+
+  characters.value = data.value; // Extract the actual data
+};
+
+const handlePageChange = async (page) => {
+  const offset = page?.page * 12;
+  const limit = (page?.page + 1) * 12;
+  initialRequest(limit, offset);
+};
+
+initialRequest();
 </script>
 
 <style>
