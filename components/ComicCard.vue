fa1635401abc9d5a68b4666ddd9ed30f2a71c860
Erfan nejat <abnoseek@gmail.com>
2025-03-06 12:01:12 +0330

    style: remove min width

--- components/ComicCard.vue
+++ components/ComicCard.vue
@@ -14,7 +14,7 @@ const handleImageError = (event) => {
 
 <template>
   <article
-    class="box-border flex flex-col items-start p-4 gap-4 w-full min-w-[20rem] min-h-[29rem] max-h-[33.8rem] border border-[#404244] rounded-[16px] mx-auto hover:shadow-lg transition-shadow"
+    class="box-border flex flex-col items-start p-4 gap-4 w-full min-h-[29rem] max-h-[33.8rem] border border-[#404244] rounded-[16px] mx-auto hover:shadow-lg transition-shadow"
     aria-labelledby="item-name"
   >
     <img
